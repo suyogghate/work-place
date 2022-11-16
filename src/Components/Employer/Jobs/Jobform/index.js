59a7556ca70abfbfb18371cbec2e1d5fc4569262
Ghate Suyog <suyogghateminks@gmail.com>
2022-11-16 23:34:02 +0530

    made jobform and sidebar inside employer/jobs section, made index.js of jobs section, made index.js of jobform

---
+++ src/Components/Employer/Jobs/Jobform/index.js
@@ -0,0 +1,230 @@
+import { Button, Chip, FormControl, Grid, InputLabel, MenuItem, OutlinedInput, Select, TextField, useTheme } from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+
+const domain = [
+  "Frontend",
+  "Backend",
+  "Fullstack",
+  "Devops",
+  "UI/UX",
+  "Machine Learning",
+  "Artificial Intelligence",
+  "Data Science",
+  "QA",
+  "Cloud Computing",
+  "BlockChain",
+  "Software Enginner",
+];
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+const skills = [
+  "C",
+  "C++",
+  "Java",
+  "Python",
+  "HTML",
+  "CSS",
+  "Javascript",
+  "React",
+  "Redux",
+  "SQL",
+  "MongoDB",
+  "Express",
+  "NodeJs",
+];
+
+function getStyles(name, personName, theme) {
+  return {
+    fontWeight:
+      personName.indexOf(name) === -1
+        ? theme.typography.fontWeightRegular
+        : theme.typography.fontWeightMedium,
+  };
+}
+
+function Jobform() {
+  const theme = useTheme();
+
+  const [jobData, setJobData] = React.useState({
+    title: "",
+    description: "",
+    location: "",
+    salary: "",
+    experience: "",
+    skills: [],
+    jobType: "",
+    domain: "",
+  });
+
+  const handleSkillChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setJobData({
+      ...jobData,
+      skills: typeof value === "string" ? value.split(",") : value,
+    });
+  };
+
+  const submitJob = () => {
+    console.log(jobData);
+  }
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <h1>Jobform</h1>
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <TextField 
+          value={jobData.title}
+          // disabled={!edit}
+          required
+          id="outlined-required"
+          label="Title"
+          fullWidth
+          sx={{ marginTop: "20px" }}
+          onChange={(e) => setJobData({...jobData, title: e.target.value})}
+        />
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <TextField
+          value={jobData.location}
+          // disabled={!edit}
+          required
+          id="outlined-required"
+          label="Location"
+          fullWidth
+          sx={{ marginTop: "20px" }}
+          onChange={(e) => setJobData({ ...jobData, location: e.target.value })}
+        />
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <TextField
+          value={jobData.salary}
+          // disabled={!edit}
+          required
+          id="outlined-required"
+          label="Salary"
+          fullWidth
+          sx={{ marginTop: "20px" }}
+          onChange={(e) => setJobData({ ...jobData, salary: e.target.value })}
+        />
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <TextField
+          value={jobData.experience}
+          // disabled={!edit}
+          required
+          id="outlined-required"
+          label="Experience"
+          fullWidth
+          sx={{ marginTop: "20px" }}
+          onChange={(e) => setJobData({...jobData, experience: e.target.value})}
+        />  
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <TextField
+          value={jobData.jobType}
+          // disabled={!edit}
+          required
+          id="outlined-required"
+          label="Job Type"
+          fullWidth
+          sx={{ marginTop: "20px" }}
+          onChange={(e) => setJobData({...jobData, jobType: e.target.value})}
+        />  
+      </Grid>
+      <Grid item xs={12} sm={12}>
+        <TextField
+        required
+          multiline
+          rows={4}
+          label="Description about the company...."
+          value={jobData.description}
+          onChange={(e) =>
+            setJobData({ ...jobData, description: e.target.value })
+          }
+          fullWidth
+        />
+      </Grid>
+      <Grid item xs={12} sm={6}>
+          <FormControl fullWidth>
+            <InputLabel
+              id="demo-simple-select-label"
+              sx={{ marginTop: "20px" }}
+            >
+              Domain
+            </InputLabel>
+            <Select
+              // disabled={!edit}
+              labelId="demo-simple-select-label"
+              id="outlined-required"
+              value={jobData.domain}
+              label="Domain"
+              sx={{ marginTop: "20px" }}
+              onChange={(e) =>
+                setJobData({ ...jobData, domain: e.target.value })
+              }
+            >
+              {domain.map((domain, index) => {
+                return (
+                  <MenuItem key={index} value={domain}>
+                    {domain}
+                  </MenuItem>
+                );
+              })}
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <FormControl sx={{ mx: 0.5, mt: 2.5, width: 500 }}>
+            <InputLabel id="demo-multiple-chip-label">Skills</InputLabel>
+            <Select
+              fullWidth
+              // disabled={!edit}
+              labelId="demo-multiple-chip-label"
+              id="demo-multiple-chip"
+              multiple
+              value={jobData.skills}
+              onChange={handleSkillChange}
+              input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
+              renderValue={(selected) => (
+                <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
+                  {selected.map((value) => (
+                    <Chip key={value} label={value} />
+                  ))}
+                </Box>
+              )}
+              MenuProps={MenuProps}
+            >
+              {skills.map((skill) => (
+                <MenuItem
+                  key={skill}
+                  value={skill}
+                  style={getStyles(skill, jobData.skills, theme)}
+                >
+                  {skill}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={12} sm={12}>
+            <Button onClick={submitJob} variant="contained" color="primary">Submit</Button>
+        </Grid>
+      </Grid>
+  );
+}
+
+export default Jobform;
--- src/Components/Employer/Jobs/index.js
+++ src/Components/Employer/Jobs/index.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import Sidebar from "../../Employer/Jobs/Sidebar";
+import Jobform from "../../Employer/Jobs/Jobform";
+import { Button, Grid } from "@mui/material";
 
 function Jobs() {
+  const [mobileSidebar, setMobileSidebar] = React.useState(true);
   return (
-    <div>Jobs</div>
-  )
+    <>
+      <Grid container spacing={2}>
+        <Grid
+          item
+          xs={12}
+          sm={3}  // 40%
+          sx={{
+            display: { xs: mobileSidebar ? "block" : "none", sm: 'block' },
+          }}
+        >
+          <Sidebar />
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          md={9} // 60%
+          sx={{
+            display: { xs: mobileSidebar ? "none" : "block", sm: 'block' },
+          }}
+        >
+          <Jobform />
+        </Grid>
+      </Grid>
+      <Button onClick={() => setMobileSidebar(!mobileSidebar)}>Switch</Button>
+    </>
+  );
 }
 
-export default Jobs
+export default Jobs;
